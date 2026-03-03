16c85297fbf39f53c15b852e0fee3cc79f3de808
ligoudan5243 <150013646+ligoudan5243@users.noreply.github.com>
2026-03-03 16:37:40 +0800

    Add files via upload

---
+++ templates/admin/adminHeader.js
@@ -0,0 +1,7 @@
+// templates/admin/adminHeader.js
+export const adminHeaderHTML = `
+<div class="admin-header">
+    <h1 class="section-title">管理面板</h1>
+    <div class="back-home-btn" id="backHomeBtn"><i class="fas fa-arrow-left"></i> 返回首页</div>
+</div>
+`;
